112168d5bdc8e2a59a0951dd04423cc8cd6cfe14
OmarSh0 <omarua007@gmail.com>
2022-12-08 17:33:08 +0200

    Scroll Adjustment

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 let topBtn = document.getElementById('top-btn');
-
 menu.onclick = () =>{
 
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
+}
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
 }
 
 topBtn.addEventListener('click', ()=> {
